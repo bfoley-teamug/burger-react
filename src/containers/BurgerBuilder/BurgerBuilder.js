e879c2abd37328fdd3b8c6376d91a15de3119b0c
Brian Foley <bfoley-teamug@users.noreply.github.com>
2018-08-29 04:27:42 -0700

    add css

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>
-          Burger
-        </div>
+        <Burger /> 
         <div>
           Build Controls
         </div>
@@ -17,4 +16,4 @@ class BurgerBuilder extends Component {
   }
 
 }
-export default BurgerBuilder; 
+export default BurgerBuilder;
