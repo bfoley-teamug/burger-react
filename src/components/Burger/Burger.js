1bb08de05ac216d6976fd5a8dfcd75f51489d399
Brian Foley <bfoley-teamug@users.noreply.github.com>
2018-09-12 04:28:59 -0700

    add ingredients array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
 import classes from './Burger.css';
-import BurgerIngredient from './Burgeringredient/Burgeringredient';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  //to turn ingredients object into array and then map
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    //arrays within arrays, use reduce to flatten arrays
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please add burger things to your burger</p>
+    }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
-    </div>      
+    </div>
 
   );
 }
